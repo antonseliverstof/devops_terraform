f46fe454b3c9e4fc2628ab8ecb679780c5ad6af4
Anton Seliverstov <anton.seliverstof@gmail.com>
2021-11-27 19:35:55 +0300

    added SG

--- main.tf
+++ main.tf
@@ -45,3 +45,34 @@ resource "aws_route_table_association" "a-rtb-subnet" {
     subnet_id = aws_subnet.myapp-subnet-1.id
     route_table_id = aws_route_table.myapp-route-table.id
 }
+
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+      "Name" = "${var.env_prefix}-sg"
+    }
+}
