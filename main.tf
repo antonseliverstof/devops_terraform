f7ceaaa32e451a4c89ccdb70d693718828afd552
Anton Seliverstov <anton.seliverstof@gmail.com>
2021-11-27 19:18:50 +0300

    added rtb and igw

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+    region = "us-east-2"
 }
 
 variable "vpc_cidr_block" {}
@@ -8,17 +8,35 @@ variable "env_prefix" {}
 variable "az" {}
 
 resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name: "${var.env_prefix}-vpc"
-  }
+    cidr_block = var.vpc_cidr_block
+    tags = {
+     Name: "${var.env_prefix}-vpc"
+    }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.az
-  tags = {
-    Name = "${var.env_prefix}-subnet"
-  }
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.az
+    tags = {
+        Name = "${var.env_prefix}-subnet"
+    }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name = "${var.env_prefix}-rtb"
+    }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name = "${var.env_prefix}-igw"
+    }
 }
